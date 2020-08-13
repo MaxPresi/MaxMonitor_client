1d129188b1f3ee9a28fb0b120c3b029989709ae7
Raphael Aracelli <34844981+MaxPresi@users.noreply.github.com>
2020-08-13 17:54:36 -0300

    Criação do código para informação do Sistema Operacional

---
+++ Server Verify/Métodos/OSystem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxMonitor.Métodos
+{
+    class OSystem
+    {
+        static string PCName = Environment.MachineName;
+        public static string GetOSType()
+        {
+            if (Environment.Is64BitOperatingSystem == true) return "64 bits";
+            else return "32 bits";
+        }
+    }
+}
