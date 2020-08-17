f9b26d4e6b4c2839ae5492422e0c1b38ab29cc8a
Raphael Aracelli <34844981+MaxPresi@users.noreply.github.com>
2020-08-17 15:10:09 -0300

    Inclusão do código para listar usuários que logaram na máquina

--- Server Verify/Métodos/OSystem.cs
+++ Server Verify/Métodos/OSystem.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.DirectoryServices;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +10,26 @@ namespace MaxMonitor.Métodos
 {
     class OSystem
     {
-        static string PCName = Environment.MachineName;
-        public static string GetOSType()
+        public static string Bits()
         {
             if (Environment.Is64BitOperatingSystem == true) return "64 bits";
             else return "32 bits";
+        }
+
+        public static List<string> Users()
+        {
+            var LstUsers = new List<string>();
+            DirectoryEntry localMachine = new DirectoryEntry("WinNT://" + Environment.MachineName);
+            DirectoryEntry admGroup = localMachine.Children.Find("users", "group");
+            object members = admGroup.Invoke("members", null);
+
+            foreach (object groupMember in (IEnumerable)members)
+            {
+                DirectoryEntry member = new DirectoryEntry(groupMember);
+                LstUsers.Add(member.Name);
+            }
+
+            return LstUsers;
         }
     }
 }
