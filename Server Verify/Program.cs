5b4398994b924c1844e01aab6475ca41c417a772
Raphael Aracelli <34844981+MaxPresi@users.noreply.github.com>
2020-08-17 15:07:27 -0300

    Mudança na forma que o programa abre para o System Tray Agora quando clicar em fechar/ok o programa minimiza para o system tray, só fechando pelo context menu

--- Server Verify/Program.cs
+++ Server Verify/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MaxMonitor.Controles;
 
 namespace Server_Verify
 {
@@ -16,7 +17,24 @@ namespace Server_Verify
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            using (NotifyIcon icon = new NotifyIcon())
+            {
+                icon.Icon = System.Drawing.Icon.ExtractAssociatedIcon(Application.ExecutablePath);
+                icon.MouseDoubleClick += MouseDoubleClick;
+                icon.ContextMenu = new ContextMenu(new MenuItem[] {
+                new MenuItem("Forçar Atualização", (s, e) => {new About().Show();}),
+                new MenuItem("Sobre...", (s, e) => {new About().Show();}),
+                new MenuItem("Exit", (s, e) => { Application.Exit(); }),
+            });
+                icon.Visible = true;
+
+                Application.Run();
+                icon.Visible = false;
+            }
+        }
+        private static void MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            new About().Show();
         }
     }
 }
